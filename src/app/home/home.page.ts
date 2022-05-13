7bf7751a81dc730ffba68ff75cb7c915c3fa4075
arsh019 <arshiyan.s@reapmind.com>
2022-05-13 12:52:27 +0530

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,74 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+  indeterminateState: boolean;
+  checkParent: boolean;
+  Checkboxes: any;
+  selectedArray: any = [];
+  constructor() {
+    this.Checkboxes = [
+      {
+        _id: 1,
+        value: "Captain Marvel",
+        isItemChecked: false
+      },
+      {
+        _id: 2,
+        value: "Thor",
+        isItemChecked: false
+      },
+      {
+        _id: 3,
+        value: "Iron Man",
+        isItemChecked: false
+      },
+      {
+        _id: 4,
+        value: "Spider Man",
+        isItemChecked: false
+      },
+      {
+        _id: 5,
+        value: "Loki",
+        isItemChecked: false
+      }
+    ];
+  }
+  checkCheckbox(ev: any) {
+    setTimeout(() => {
+      this.Checkboxes.forEach(item => {
+        item.isItemChecked = this.checkParent;
+        console.log('item.isItemChecked :', item.isItemChecked);
+      });
+    });
+  }
+  verifyEvent(ev: any, check: any) {
+    if (check.isItemChecked) {
+      this.selectedArray.push(check);
+    }
+    else {
+      this.selectedArray.splice(this.selectedArray.indexOf(check), 1);
+    }
+    console.log('this.selectedArray :', this.selectedArray);
+    const allItems = this.Checkboxes.length;
+    let selected = 0;
+    this.Checkboxes.map(item => {
+      if (item.isItemChecked) selected++;
+    });
+    if (selected > 0 && selected < allItems) {
+      /* One item is selected among all checkbox elements */
+      this.indeterminateState = true;
+      this.checkParent = false;
+    } else if (selected == allItems) {
+      /* All item selected */
+      this.checkParent = true;
+      this.indeterminateState = false;
+    } else {
+      /* No item is selected */
+      this.indeterminateState = false;
+      this.checkParent = false;
+    }
+  }
 
 }
